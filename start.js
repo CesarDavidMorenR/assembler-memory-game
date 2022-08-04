19bfcbe1e2914ec8602b7ea84832e42856a2ec8c
David Moreno <cesardaviddev@gmail.com>
2022-08-04 10:31:19 +0200

    Basic JS Max Widt body some css

---
+++ start.js
@@ -0,0 +1,11 @@
+/* David - Begin */
+
+let btnStart = document.getElementById("btn-start");
+
+btnStart.addEventListener("click", () => {
+  let mainBody = document.querySelector(".main-dad");
+
+  mainBody.style.display = "none";
+});
+
+/* David -End */
